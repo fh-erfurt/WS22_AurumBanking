0a586e9328e3ad0eb15af861b1f60f553414affa
FalcarDE <hoangtrananh1234@gamil.com>
2023-01-24 11:32:16 +0100

    init branch

--- app/src/main/java/de/fhe/ai/aurumbanking/view/deposit/DepositFragment.kt
+++ app/src/main/java/de/fhe/ai/aurumbanking/view/deposit/DepositFragment.kt
@@ -11,16 +11,19 @@ import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import de.fhe.ai.aurumbanking.R
 import de.fhe.ai.aurumbanking.core.Helper
 import java.math.BigDecimal
 
 class DepositFragment : Fragment(){
 
+    val ARG_TRANSACTIONLISTID_ID = "contactId"
     private var customerId: Long? = null
     private var helper: Helper = Helper.getHelperInstance()
     private lateinit var root: View
     private lateinit var viewModel: DepositViewModel
+
 
     @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("SetTextI18n")
@@ -44,7 +47,19 @@ class DepositFragment : Fragment(){
         val currentDepotCountry = this.root.findViewById<ImageView>(R.id.userDepotRight)
         currentDepotCountry.setImageResource(R.drawable.europaische_union)
 
-      
+
+        // Get RecyclerView Reference
+
+        // Get RecyclerView Reference
+        val latestDepotTransaction = root.findViewById<RecyclerView>(R.id.latestDepotTransaction)
+
+        val adapter : TransactionListAdapter = TransactionListAdapter(requireActivity(), transactionListId {
+            var args : Bundle = Bundle()
+            args.putLong(ARG_TRANSACTIONLISTID_ID ,transactionListId)
+
+        })
+
+
         return this.root
 
     }
